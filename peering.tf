ac638390a6a093b1d85eae9ef768791acdd95ecd
narendrakumar-sun <narendra.sunkara83@gmail.com>
2026-04-09 11:50:09 +0400

    action

--- peering.tf
+++ peering.tf
@@ -34,14 +34,14 @@ resource "aws_route" "public_peering" {
 resource "aws_route" "private_peering" {
   count = var.peering_requried ? 1 :0
   route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = data.aws_route_table.default.cidr_block
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
 resource "aws_route" "database_peering" {
   count = var.peering_requried ? 1 :0
   route_table_id            = aws_route_table.databace.id
-  destination_cidr_block    = data.aws_route_table.default.cidr_block
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
